b3c9f5c99cb59ca0aaecdc238ba637ddcd2bc0c7
Matt Rosanio <matt.rosanio@gmail.com>
2020-10-12 12:08:27 -0700

    Add audio source

---
+++ Assets/Rocket.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    new Rigidbody rigidbody;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        // Thrust
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+
+        // Rotation
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward, 30 * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward, 30 * Time.deltaTime);
+        }
+    }
+}
